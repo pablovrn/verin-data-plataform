af70b44ab464ad341747fba998e452afcbf13ce7
Pablo Fernández <pablo@MacBook-Air-de-Pablo.local>
2026-04-13 19:09:09 +0200

    Ampliado el datamart de Poblacion. Retoques en la web

--- etl/carga_poblacion.py
+++ etl/carga_poblacion.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
+from sqlalchemy import text
 
 load_dotenv(override=True)
 
@@ -101,3 +102,144 @@ df_to_insert = df_merged[df_merged['_merge'] == 'left_only'].drop(columns=['_mer
 # Insertar/actualizar en la tabla fact_nacimientos
 df_to_insert.to_sql('fact_nacimientos', engine, schema='verin_dw', if_exists='append', index=False)
 
+"""
+ETL para la tabla fact_defunciones en la base de datos verin_dw.
+Fuente de datos: Instituto Galego de Estatística (IGE)
+"""
+
+url = "https://www.ige.gal/igebdt/igeapi/csv/datos/30/9915:32021:32028:32039:32050:32053:32071:32085:32091"
+
+# Leer CSV directamente desde la web
+df = pd.read_csv(url, sep=',', encoding='latin1', header=0)
+
+#Leer datos de 2000 en adelante
+df = df[df['Tempo'] >= 2000]
+
+# Borrar columnas innecesarias
+df = df.drop(columns=['CodTempo', 'Espazo', 'DatoT'])
+
+# Si datoN es NaN, rellenar con -1 para indicar que no hay datos disponibles
+df['DatoN'] = df['DatoN'].fillna(-1)
+
+# Pivotar el DataFrame para tener una fila por combinación de Tempo y Espazo, y columnas para cada Sexo
+df_pivot = df.pivot_table(index=['Tempo', 'CodEspazo'], columns='Sexo', values='DatoN').reset_index()
+df_pivot.rename(columns={'Tempo': 'id_fecha', 'CodEspazo': 'id_municipio', 'Homes': 'hombres', 'Mulleres': 'mujeres', 'Total': 'defunciones_total'}, inplace=True)
+
+# Convertir id_fecha a formato DATE
+df_pivot['id_fecha'] = pd.to_datetime(df_pivot['id_fecha'], format='%Y').dt.date
+
+# Hacer un merge para insertar solo nuevos datos
+existing_data = pd.read_sql('SELECT id_fecha, id_municipio FROM verin_dw.fact_defunciones', engine)
+df_merged = df_pivot.merge(existing_data, on=['id_fecha', 'id_municipio'], how='left', indicator=True)
+df_to_insert = df_merged[df_merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+# Insertar/actualizar en la tabla fact_defunciones
+df_to_insert.to_sql('fact_defunciones', engine, schema='verin_dw', if_exists='append', index=False)
+
+"""
+ETL para la tabla fact_poblacion_edad en la base de datos verin_dw.
+Fuente de datos: Instituto Galego de Estatística (IGE)
+"""
+
+url = "https://www.ige.gal/igebdt/igeapi/csv/datos/142/2:1:2:3:4:5:6:7:8:9:10:11:12:13:14:15:16:17:18:22,9915:32021:32028:32039:32050:32053:32071:32085:32091"
+
+# Leer CSV directamente desde la web
+df = pd.read_csv(url, sep=',', encoding='latin1', header=0)
+
+#Leer datos de 2000 en adelante
+df = df[df['Tempo'] >= 2000]
+
+# Borrar columnas innecesarias
+df = df.drop(columns=['CodTempo', 'Espazo', 'DatoT'])
+
+# Si datoN es NaN, rellenar con -1 para indicar que no hay datos disponibles
+df['DatoN'] = df['DatoN'].fillna(-1)
+
+#Recuperar los grupos de edad actuales del DW
+edades_dw = pd.read_sql('SELECT * FROM verin_dw.dim_grupo_edad', engine)
+
+# Leer la columna "Grupos de idade" para rellenar la dimensión de edad con los grupos de edad nuevos
+grupos_edad = df['Grupos de idade'].unique()
+for grupo in grupos_edad:
+    if grupo not in edades_dw['rango'].values:
+        with engine.connect() as connection:
+            connection.execute(text("INSERT INTO verin_dw.dim_grupo_edad (rango) VALUES (:grupo)"), {"grupo": grupo})
+            connection.commit()
+
+#Recuperar los grupos de edad con sus IDs actualizados
+edades_dw = pd.read_sql('SELECT * FROM verin_dw.dim_grupo_edad', engine)
+
+#Mapear el grupo de edad con su ID correspondiente
+df = df.merge(edades_dw, left_on='Grupos de idade', right_on='rango', how='left')
+
+#Eliminar columnas innecesarias
+df = df.drop(columns=['Grupos de idade', 'rango'])
+
+# Pivotar el DataFrame para tener una fila por combinación de Tempo, Espazo y grupo de edad, y columnas para cada Sexo
+df_pivot = df.pivot_table(index=['Tempo', 'CodEspazo', 'id_grupo_edad'], columns='Sexo', values='DatoN').reset_index()
+df_pivot.rename(columns={'Tempo': 'id_fecha', 'CodEspazo': 'id_municipio', 'Homes': 'hombres', 'Mulleres': 'mujeres', 'Total': 'poblacion_total'}, inplace=True)
+
+# Convertir id_fecha a formato DATE
+df_pivot['id_fecha'] = pd.to_datetime(df_pivot['id_fecha'], format='%Y').dt.date
+
+# Hacer un merge para insertar solo nuevos datos
+existing_data = pd.read_sql('SELECT id_fecha, id_municipio FROM verin_dw.fact_poblacion_edad', engine)
+df_merged = df_pivot.merge(existing_data, on=['id_fecha', 'id_municipio'], how='left', indicator=True)
+df_to_insert = df_merged[df_merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+# Insertar/actualizar en la tabla fact_poblacion_edad
+df_to_insert.to_sql('fact_poblacion_edad', engine, schema='verin_dw', if_exists='append', index=False)
+
+"""
+ETL para la tabla fact_poblacion_lugar en la base de datos verin_dw.
+Fuente de datos: Instituto Galego de Estatística (IGE)
+"""
+
+url = "https://www.ige.gal/igebdt/igeapi/csv/datos/143/2:1:2:3:4:5,9915:32021:32028:32039:32050:32053:32071:32085:32091"
+
+# Leer CSV directamente desde la web
+df = pd.read_csv(url, sep=',', encoding='latin1', header=0)
+
+#Leer datos de 2000 en adelante
+df = df[df['Tempo'] >= 2000]
+
+# Borrar columnas innecesarias
+df = df.drop(columns=['CodTempo', 'Espazo', 'DatoT'])
+
+# Si datoN es NaN, rellenar con -1 para indicar que no hay datos disponibles
+df['DatoN'] = df['DatoN'].fillna(-1)
+
+#Recuperar los grupos de edad actuales del DW
+edades_dw = pd.read_sql('SELECT * FROM verin_dw.dim_lugar_nacimiento', engine)
+
+# Leer la columna "Lugar de nacemento" para rellenar la dimensión de lugar de nacimiento con los lugares de nacimiento nuevos
+grupos_edad = df['Lugar de nacemento'].unique()
+for grupo in grupos_edad:
+    if grupo not in edades_dw['nombre'].values:
+        with engine.connect() as connection:
+            connection.execute(text("INSERT INTO verin_dw.dim_lugar_nacimiento (nombre) VALUES (:grupo)"), {"grupo": grupo})
+            connection.commit()
+
+#Recuperar los grupos de edad con sus IDs actualizados
+edades_dw = pd.read_sql('SELECT * FROM verin_dw.dim_lugar_nacimiento', engine)
+
+#Mapear el grupo de edad con su ID correspondiente
+df = df.merge(edades_dw, left_on='Lugar de nacemento', right_on='nombre', how='left')
+
+#Eliminar columnas innecesarias
+df = df.drop(columns=['Lugar de nacemento', 'nombre'])
+
+# Pivotar el DataFrame para tener una fila por combinación de Tempo, Espazo y grupo de edad, y columnas para cada Sexo
+df_pivot = df.pivot_table(index=['Tempo', 'CodEspazo', 'id_lugar_nacimiento'], columns='Sexo', values='DatoN').reset_index()
+df_pivot.rename(columns={'Tempo': 'id_fecha', 'CodEspazo': 'id_municipio', 'Homes': 'hombres', 'Mulleres': 'mujeres', 'Total': 'poblacion_total'}, inplace=True)
+
+# Convertir id_fecha a formato DATE
+df_pivot['id_fecha'] = pd.to_datetime(df_pivot['id_fecha'], format='%Y').dt.date
+
+# Hacer un merge para insertar solo nuevos datos
+existing_data = pd.read_sql('SELECT id_fecha, id_municipio FROM verin_dw.fact_poblacion_lugar', engine)
+df_merged = df_pivot.merge(existing_data, on=['id_fecha', 'id_municipio'], how='left', indicator=True)
+df_to_insert = df_merged[df_merged['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+# Insertar/actualizar en la tabla fact_poblacion_lugar
+df_to_insert.to_sql('fact_poblacion_lugar', engine, schema='verin_dw', if_exists='append', index=False)
--- sql/datamarts/poblacion.sql
+++ sql/datamarts/poblacion.sql
@@ -21,3 +21,60 @@ CREATE TABLE IF NOT EXISTS verin_dw.fact_nacimientos (
     FOREIGN KEY (id_fecha) REFERENCES verin_dw.dim_fecha(id_fecha),
     FOREIGN KEY (id_municipio) REFERENCES verin_dw.dim_municipio(id_municipio)
 );
+
+-- Tabla de hechos de defunciones
+CREATE TABLE IF NOT EXISTS verin_dw.fact_defunciones (
+    id_fecha DATE NOT NULL,
+    id_municipio INT NOT NULL,
+    defunciones_total INT,
+    hombres INT,
+    mujeres INT,
+    PRIMARY KEY (id_fecha, id_municipio),
+    FOREIGN KEY (id_fecha) REFERENCES verin_dw.dim_fecha(id_fecha),
+    FOREIGN KEY (id_municipio) REFERENCES verin_dw.dim_municipio(id_municipio)
+);
+
+-- ID para la dimensión de grupo de edad
+CREATE SEQUENCE IF NOT EXISTS verin_dw.grupo_edad_id_seq START 1;
+-- Dimension grupo de edad
+CREATE TABLE IF NOT EXISTS verin_dw.dim_grupo_edad (
+    id_grupo_edad INT PRIMARY KEY DEFAULT nextval('verin_dw.grupo_edad_id_seq'),
+    rango VARCHAR(20) NOT NULL UNIQUE
+);
+
+-- Tabla de hechos de población por edad
+CREATE TABLE IF NOT EXISTS verin_dw.fact_poblacion_edad (
+    id_fecha DATE NOT NULL,
+    id_municipio INT NOT NULL,
+    id_grupo_edad INT NOT NULL,
+    poblacion_total INT,
+    hombres INT,
+    mujeres INT,
+    PRIMARY KEY (id_fecha, id_municipio, id_grupo_edad),
+    FOREIGN KEY (id_fecha) REFERENCES verin_dw.dim_fecha(id_fecha),
+    FOREIGN KEY (id_municipio) REFERENCES verin_dw.dim_municipio(id_municipio),
+    FOREIGN KEY (id_grupo_edad) REFERENCES verin_dw.dim_grupo_edad(id_grupo_edad)
+);
+
+-- Id para la dimensión de lugar de nacimiento
+CREATE SEQUENCE IF NOT EXISTS verin_dw.lugar_nacimiento_id_seq START 1;
+-- Dimensión para lugar de nacimiento
+CREATE TABLE IF NOT EXISTS verin_dw.dim_lugar_nacimiento (
+    id_lugar_nacimiento INT PRIMARY KEY DEFAULT nextval('verin_dw.lugar_nacimiento_id_seq'),
+    nombre VARCHAR(100) NOT NULL UNIQUE
+);
+-- Tabla de hechos de población por lugar de nacimiento
+CREATE TABLE IF NOT EXISTS verin_dw.fact_poblacion_lugar (
+    id_fecha DATE NOT NULL,
+    id_municipio INT NOT NULL,
+    id_lugar_nacimiento INT NOT NULL,
+    poblacion_total INT,
+    hombres INT,
+    mujeres INT,
+    PRIMARY KEY (id_fecha, id_municipio, id_lugar_nacimiento),
+    FOREIGN KEY (id_fecha) REFERENCES verin_dw.dim_fecha(id_fecha),
+    FOREIGN KEY (id_municipio) REFERENCES verin_dw.dim_municipio(id_municipio),
+    FOREIGN KEY (id_lugar_nacimiento) REFERENCES verin_dw.dim_lugar_nacimiento(id_lugar_nacimiento)
+);
+
+
